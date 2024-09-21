44b29c1796ed1c94da6eb9e433113bd726b7d364
gadielsampaio <marcosgadielsampaio@gmail.com>
2024-09-21 08:43:47 -0400

    core: add nextauth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,10 +1,18 @@
-import { CalendarDaysIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarDaysIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
-import { Avatar, AvatarImage } from "./ui/avatar"
+// import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import Image from "next/image"
 import { Button } from "./ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -12,9 +20,37 @@ const SidebarSheet = () => {
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
-
-      <div className="flex items-center gap-2 border-b border-solid py-5">
-        <Avatar>
+      {/* PROFILE */}
+      <div className="flex items-center justify-between gap-2 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, Faça seu Login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%] rounded-sm">
+            <DialogHeader className="mb-2">
+              <DialogTitle>Fazer Login</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta Gooogle
+              </DialogDescription>
+            </DialogHeader>
+            <Button
+              className="mx-auto my-0 size-fit gap-2 font-bold"
+              variant="outline"
+            >
+              <Image
+                src="/Google.svg"
+                width={18}
+                height={18}
+                alt="Fazer login com o Google"
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage
             className="object-cover"
             src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -24,9 +60,9 @@ const SidebarSheet = () => {
         <div>
           <p className="font-bold">Marcos Gadiel</p>
           <p className="text-xs">gadiel@barber.com</p>
-        </div>
+        </div> */}
       </div>
-
+      {/* MENU */}
       <div className="flex flex-col gap-2 border-b border-solid py-5">
         <Button className="justify-start gap-2" variant="ghost" asChild>
           <SheetClose asChild>
